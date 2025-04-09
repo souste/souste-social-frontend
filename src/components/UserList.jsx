b569f579709ad2d79d355234659df3a9395ce9c3
Stephen <stephensouness@outlook.com>
2025-04-09 16:09:04 +0100

    provisional styling applied to UserList component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -19,20 +19,30 @@ const UserList = () => {
     fetchProfiles();
   }, []);
   return loading ? (
-    <p>User List is loading</p>
+    <div>
+      <p>User List is loading</p>
+    </div>
   ) : (
     <div>
-      <ul>
+      <ul className="divide-y divide-gray-100">
         {profiles.map((profile) => {
           return (
-            <li key={profile.user_id}>
-              <img
-                src={profile.picture}
-                alt={`${profile.first_name}'s profile picture`}
-              />
-              <p>
-                {profile.first_name} {profile.last_name}
-              </p>
+            <li
+              key={profile.user_id}
+              className="flex cursor-pointer items-center gap-2 p-4 transition-colors hover:bg-gray-50"
+            >
+              <div className="flex-shrink-0">
+                <img
+                  src={profile.picture}
+                  alt={`${profile.first_name}'s profile picture`}
+                  className="h-10 w-10 rounded-full border border-gray-200 object-cover"
+                />
+              </div>
+              <div className="text-sm font-medium text-gray-900">
+                <p>
+                  {profile.first_name} {profile.last_name}
+                </p>
+              </div>
             </li>
           );
         })}
