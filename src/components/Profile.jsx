4c9b9b40a3b456c05bc1f527ea7dc59608ffdb3f
Stephen <stephensouness@outlook.com>
2025-04-09 13:10:32 +0100

    added back button to Profile page and temporary userList link button to Header component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -102,6 +102,14 @@ const Profile = () => {
           </p>
         </div>
       </div>
+      <div className="mt-6 flex justify-center gap-6">
+        <button
+          onClick={() => navigate("/")}
+          className="hover: focus:-red-300 mt-10 inline-block cursor-pointer rounded-full bg-gray-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-red-300"
+        >
+          Back
+        </button>
+      </div>
     </div>
   );
 };
