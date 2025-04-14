244d0872919c5fe42cdd33df5cd50c797c5914a3
Stephen <stephensouness@outlook.com>
2025-04-14 14:11:05 +0100

    cancel friend button added

--- src/components/FriendRequestButton.jsx
+++ src/components/FriendRequestButton.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { sendRequest, getFriendStatus } from "../api/friend";
+import { sendRequest, getFriendStatus, cancelRequest } from "../api/friend";
 
 const FriendRequestButton = ({ userId, friendId }) => {
   const [status, setStatus] = useState("none");
@@ -21,19 +21,40 @@ const FriendRequestButton = ({ userId, friendId }) => {
     try {
       await sendRequest(userId, friendId);
       setStatus("pending");
+      window.confirm("Friend Request Sent");
     } catch (err) {
       console.error("Failed to send friend request", err);
     }
   };
 
+  const handleCancelRequest = async () => {
+    try {
+      await cancelRequest(userId, friendId);
+      setStatus("none");
+      window.confirm("Friend Request Cancelled");
+    } catch (err) {
+      console.error("Failed to cancel friend request", err);
+    }
+  };
+
   return (
     <div>
-      <button
-        onClick={handleSendRequest}
-        className="mt-10 inline-block cursor-pointer rounded-full bg-blue-500 px-4 py-3 font-semibold tracking-wide text-white uppercase transition-colors duration-300 hover:bg-blue-600"
-      >
-        Add Friend
-      </button>
+      {status === "none" && (
+        <button
+          onClick={handleSendRequest}
+          className="mt-10 inline-block cursor-pointer rounded-full bg-blue-500 px-4 py-3 font-semibold tracking-wide text-white uppercase transition-colors duration-300 hover:bg-blue-600"
+        >
+          Add Friend
+        </button>
+      )}
+      {status === "pending" && (
+        <button
+          onClick={handleCancelRequest}
+          className="mt-10 inline-block cursor-pointer rounded-full bg-rose-500 px-4 py-3 font-semibold tracking-wide text-white uppercase transition-colors duration-300 hover:bg-rose-600"
+        >
+          Cancel Request
+        </button>
+      )}
     </div>
   );
 };
