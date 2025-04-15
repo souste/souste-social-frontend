7699ec0df08c81f3f2890d0428d9314834dda507
Stephen <stephensouness@outlook.com>
2025-04-15 15:55:53 +0100

    applied grid layout with tailwind to ProfilePage: menu-left, profile-middle, friend/pendingRequests - right

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -12,18 +12,27 @@ const ProfilePage = () => {
   const isCurrentUser = userId === currentUser.id;
 
   return (
-    <div>
-      <Profile
-        profileId={userId}
-        viewerId={currentUser.id}
-        isCurrentUser={isCurrentUser}
-      />
-      {isCurrentUser && (
-        <div>
-          <FriendsList userId={userId} />
-          <PendingRequestList userId={userId} />
-        </div>
-      )}
+    <div className="mx-auto mt-8 grid max-w-7xl grid-cols-1 gap-6 px-4 sm:grid-cols-5">
+      <div className="sm:col-span-1">
+        <p>Menu Bar</p>
+      </div>
+
+      <div className="overflow-hidden rounded-lg bg-white shadow sm:col-span-3">
+        <Profile
+          profileId={userId}
+          viewerId={currentUser.id}
+          isCurrentUser={isCurrentUser}
+        />
+      </div>
+
+      <div className="sm:col-span-1">
+        {isCurrentUser && (
+          <div className="space-y-6">
+            <PendingRequestList userId={userId} />
+            <FriendsList userId={userId} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
