a811d8858d38f7c3861ff2a81e7e43550fd3f42d
Stephen <stephensouness@outlook.com>
2025-05-15 10:18:30 +0100

    Further Tailwind Styling applied to Login/Signup pages

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -44,69 +44,74 @@ const Login = () => {
   };
 
   return (
-    <div className="mx-auto max-w-lg px-4 py-6">
-      <h1 className="mb-6 text-center text-2xl font-bold text-gray-800">
-        Login
-      </h1>
-      {errors.length > 0 && (
-        <div className="mb-6 bg-red-100 p-3 text-red-700">
-          <ul>
-            {errors.map((error, index) => (
-              <li key={index}>{error.msg}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <form
-        onSubmit={handleSubmit}
-        noValidate
-        className="flex flex-col gap-4"
-      >
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="email"
-            className="text-sm font-semibold text-gray-700"
+    <div className="flex min-h-screen items-start justify-center bg-gray-100 px-4 pt-30">
+      <div className="w-full max-w-md rounded-xl bg-white p-8 shadow-lg">
+        <h1 className="mb-6 text-center text-2xl font-bold text-gray-800">
+          Login
+        </h1>
+        {errors.length > 0 && (
+          <div
+            div
+            className="mb-6 rounded-md border border-red-300 bg-red-100 p-3 text-red-700"
           >
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={loginCredentials.email}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+            <ul className="list-inside list-disc">
+              {errors.map((error, index) => (
+                <li key={index}>{error.msg}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <form
+          onSubmit={handleSubmit}
+          noValidate
+          className="flex flex-col gap-4"
+        >
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={loginCredentials.email}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="password"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={loginCredentials.password}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
-        <div className="flex flex-col sm:items-center">
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="mt-6 w-full max-w-md cursor-pointer rounded-full bg-blue-600 py-3 font-semibold text-white uppercase transition hover:bg-blue-700"
-          >
-            {isSubmitting ? "Logging in" : "Login"}
-          </button>
-        </div>
-      </form>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="password"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={loginCredentials.password}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
+          <div className="flex flex-col sm:items-center">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="mt-4 w-full cursor-pointer rounded-full bg-blue-600 py-3 font-semibold text-white uppercase transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-blue-300"
+            >
+              {isSubmitting ? "Logging in" : "Login"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -45,141 +45,143 @@ const SignUp = () => {
   };
 
   return (
-    <div className="mx-auto max-w-lg px-4 py-6">
-      <h1 className="mb-6 text-center text-2xl font-bold text-gray-800">
-        Sign Up
-      </h1>
-      {errors.length > 0 && (
-        <div className="mb-6 border-red-500 bg-red-100 p-3 text-red-700">
-          <ul>
-            {errors.map((error, index) => (
-              <li key={index}>{error.msg}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <form
-        onSubmit={handleSubmit}
-        noValidate
-        className="flex flex-col gap-4"
-      >
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="first_name"
-            className="text-sm font-semibold text-gray-700"
-          >
-            First Name:
-          </label>
-          <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            value={user.first_name}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-xl rounded-xl bg-white p-8 shadow-lg">
+        <h1 className="mb-6 text-center text-2xl font-bold text-gray-800">
+          Sign Up
+        </h1>
+        {errors.length > 0 && (
+          <div className="mb-6 rounded-md border border-red-300 bg-red-100 p-3 text-red-700">
+            <ul className="list-inside list-disc">
+              {errors.map((error, index) => (
+                <li key={index}>{error.msg}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <form
+          onSubmit={handleSubmit}
+          noValidate
+          className="flex flex-col gap-4"
+        >
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="first_name"
+              className="text-sm font-semibold text-gray-700"
+            >
+              First Name:
+            </label>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              value={user.first_name}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="last_name"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Last Name:
-          </label>
-          <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            value={user.last_name}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="last_name"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Last Name:
+            </label>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              value={user.last_name}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="username"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={user.username}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="username"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Username:
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={user.username}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="email"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={user.email}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="password"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={user.password}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="password"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
 
-        <div className="mb-3 flex flex-col gap-2 sm:items-center">
-          <label
-            htmlFor="confirm_password"
-            className="text-sm font-semibold text-gray-700"
-          >
-            Confirm Password:
-          </label>
-          <input
-            type="password"
-            id="confirm_password"
-            name="confirm_password"
-            value={user.confirm_password}
-            onChange={handleChange}
-            className="w-full max-w-md rounded-full border border-gray-300 bg-gray-100 px-4 py-2 focus:ring focus:ring-blue-600 focus:outline-none"
-            required
-          />
-        </div>
-        <div className="flex flex-col sm:items-center">
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="hover:bg-blue-4700 mt-6 w-full max-w-md cursor-pointer rounded-full bg-blue-600 py-3 font-semibold text-white uppercase transition"
-          >
-            {isSubmitting ? "Submitting" : "Sign Up"}
-          </button>
-        </div>
-      </form>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="confirm_password"
+              className="text-sm font-semibold text-gray-700"
+            >
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              id="confirm_password"
+              name="confirm_password"
+              value={user.confirm_password}
+              onChange={handleChange}
+              className="w-full rounded-full border border-gray-300 bg-gray-100 px-4 py-2 text-sm focus:ring-2 focus:ring-blue-600 focus:outline-none"
+              required
+            />
+          </div>
+          <div className="flex flex-col sm:items-center">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="mt-4 w-full cursor-pointer rounded-full bg-blue-600 py-3 font-semibold text-white uppercase transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-blue-300"
+            >
+              {isSubmitting ? "Submitting" : "Sign Up"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
